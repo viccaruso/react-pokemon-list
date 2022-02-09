c03ece932f09e6b76ad39e611e53811fcf38931c
Victor Caruso <vic.caruso@gmail.com>
2022-02-08 18:00:50 -0800

    moved capitalize function to it's own utils.js file

--- src/Pokemon.js
+++ src/Pokemon.js
@@ -1,15 +1,14 @@
 import React from 'react';
+import { capitalize } from './services/utils';
 
 export default function Pokemon({ pokemon }) {
-
-  function capitalize(word){
-    return word.charAt(0).toUpperCase() + word.slice(1);
-  }
 
   return (
     <div className='pokemon-card'>
       <h3>{ capitalize(pokemon.pokemon) }</h3>
       <img src={ pokemon.url_image }/>
+      <p>Type I: {capitalize(pokemon.type_1)}</p>
+      <p>Type II: {capitalize(pokemon.type_2)}</p>
       <p>Gen {pokemon.generation_id} </p>
     </div>
   );
