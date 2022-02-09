6eb5cded50de1fe86dc2919e227bcab586cd9340
Victor Caruso <vic.caruso@gmail.com>
2022-02-09 14:12:51 -0800

    styled background colors and using css instead of js to capitalize words

--- src/Pokemon.js
+++ src/Pokemon.js
@@ -1,15 +1,14 @@
 import React from 'react';
-import { capitalize } from './services/utils';
 
 export default function Pokemon({ pokemon }) {
 
   return (
-    <div className='pokemon-card'>
-      <h3>{ capitalize(pokemon.pokemon) }</h3>
+    <div className={`pokemon-card ${ pokemon.type_1 }-bg`}>
+      <h3>{ pokemon.pokemon }</h3>
       <img src={ pokemon.url_image }/>
-      <p>Type I: {capitalize(pokemon.type_1)}</p>
-      <p>Type II: {capitalize(pokemon.type_2)}</p>
-      <p>Gen {pokemon.generation_id} </p>
+      <p>Type I: { pokemon.type_1 }</p>
+      <p>Type II: { pokemon.type_2 }</p>
+      <p>Gen { pokemon.generation_id } </p>
     </div>
   );
 }
--- src/services/utils.js
+++
@@ -1,3 +0,0 @@
-export function capitalize(word){
-  return word.charAt(0).toUpperCase() + word.slice(1);
-}
