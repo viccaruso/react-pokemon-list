552168cea982812e970202910d7661e3aac0546f
Victor Caruso <vic.caruso@gmail.com>
2022-02-08 16:52:54 -0800

    set up state and defined a fetch function to set state in useEffect

--- src/Home.js
+++ src/Home.js
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { fetchAllPokemon } from './services/fetch-utils';
 import PokemonList from './PokemonList';
 
 
 export default function Home() {
-  
+  // set up state
+  const [pokemans, setPokemans] = useState([]);
+
+  useEffect(() => {
+    async function fetch() {
+      setPokemans(await fetchAllPokemon());
+    }
+
+    fetch();
+  }, []);
 
   return (
     <div>
-      <PokemonList pokemons={pokemons} />
+      <PokemonList pokemans={pokemans} />
     </div>
   );
 }
